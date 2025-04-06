77465a604dff3c464cb3aaa24c09978cf7845697
patwork <patwork@gmail.com>
2025-04-06 12:01:01 +0200

    added game UI

---
+++ ui/game_ui.gd
@@ -0,0 +1,19 @@
+class_name MyGameUI
+extends CanvasLayer
+
+
+@onready var control_state: Control = $Control_state
+@onready var control_win: Control = $Control_win
+@onready var control_loose: Control = $Control_loose
+@onready var label_life: Label = %Label_life
+@onready var label_ring: Label = %Label_ring
+
+
+func _ready() -> void:
+	control_state.visible = true
+	control_win.visible = false
+	control_loose.visible = false
+
+
+func _on_button_menu_pressed() -> void:
+	get_tree().change_scene_to_file(Constants.menu_scene)
